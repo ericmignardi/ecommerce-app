fe921a8a528b05cd162ab96cc5ec8419d3c8164c
Eric Mignardi <mignardi.e@gmail.com>
2025-07-01 09:59:32 -0400

    Completed frontend development

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/frontendAssets/assets";
+import { ShopContext } from "../contexts/ShopContext";
 
 const PlaceOrder = () => {
   const [method, setMethod] = useState("cod");
+  const { navigate } = useContext(ShopContext);
 
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
@@ -72,21 +74,37 @@ const PlaceOrder = () => {
           <Title text1={"PAYMENT"} text2={"METHOD"} />
           <div className="flex gap-3 flex-col lg:flex-row">
             <div
-              onClick={setMethod("stripe")}
+              onClick={() => setMethod("stripe")}
               className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
             >
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "stripe" ? "bg-green-400" : ""
+                }`}
+              ></p>
               <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
             </div>
             <div
-              onClick={setMethod("cod")}
+              onClick={() => setMethod("cod")}
               className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
             >
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "cod" ? "bg-green-400" : ""
+                }`}
+              ></p>
               <p className="text-gray-500 text-sm font-medium mx-4">
                 Cash On Delivery
               </p>
             </div>
+          </div>
+          <div className="w-full text-end mt-8">
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
+              PLACE ORDER
+            </button>
           </div>
         </div>
       </div>
